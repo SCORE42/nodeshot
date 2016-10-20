a465578e2aa95f1be8f40bcae3ef40e22f22330b
Renaud RAKOTOMALALA <renaud.rakotomalala@score42.eu>
2016-10-21 00:04:29 +0200

    update serializer dry 3.3+ dependency

--- nodeshot/community/mailing/serializers.py
+++ nodeshot/community/mailing/serializers.py
@@ -10,9 +10,10 @@ __all__ = [
 
 class InwardSerializer(serializers.ModelSerializer):
     """ status icons """
-    
+
     class Meta:
         model = Inward
         read_only_fields = (
             'status', 'added', 'updated', 'content_type', 'object_id',
             'user', 'ip', 'user_agent', 'accept_language')
+        fields = '__all__'
